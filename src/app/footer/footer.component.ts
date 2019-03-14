4883116777c012259a3e67d58abb7eda1305b76d
jmohon1986 <jmohon1986@gmail.com>
2019-03-14 10:08:06 -0500

    modals work added .env

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
+import { ModalController } from '@ionic/angular';
 import { Router } from '@angular/router'
+import { ModalsComponent } from '../modals/modals.component';
 
 @Component({
   selector: 'app-footer',
@@ -9,12 +11,24 @@ import { Router } from '@angular/router'
 })
 export class FooterComponent implements OnInit {
 
-  constructor(public authService: AuthService, private router: Router) { }
+  constructor(public authService: AuthService, private router: Router, public modal: ModalController) { }
   
   onClick(): void {
     console.log('log me out!');
     this.authService.logout();
     this.router.navigate(['/Greet']);
+  }
+  async openModal()
+  {
+
+    var data = { message : 'hello world' };
+
+    const modalPage = await this.modal.create({
+      component: ModalsComponent, 
+      componentProps:{values: data}
+    });
+
+    return await modalPage.present();
   }
   ngOnInit() {}
 
