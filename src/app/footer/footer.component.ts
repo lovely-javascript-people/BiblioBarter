6b7e9a3eb347272c3788bb34dd81da85b5ef5b84
jmohon1986 <jmohon1986@gmail.com>
2019-03-13 20:10:41 -0500

    Append footer component to app

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  onClick() {
+    console.log('hi');
+  }
   constructor() { }
 
   ngOnInit() {}
--- src/app/footer/footer.module.ts
+++ src/app/footer/footer.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [FooterComponent]
 })
 export class FooterModule { }
