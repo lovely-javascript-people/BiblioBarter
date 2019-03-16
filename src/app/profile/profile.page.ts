51f612dfd814fa33f0453d473a0056178a287e46
jmohon1986 <jmohon1986@gmail.com>
2019-03-16 14:34:00 -0500

    fixed profile bug

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -39,9 +39,13 @@ export class ProfilePage implements OnInit{
 
   setUser(data) {
     console.log(data);
+    if (data[0].length) {
     this.user = data[0].user_name;
     this.school = data[1][0].name_school;
     this.img = data[0].link_image;
+    } else {
+      this.user = localStorage.username;
+    }
   }
 
   async openWantListModal()
