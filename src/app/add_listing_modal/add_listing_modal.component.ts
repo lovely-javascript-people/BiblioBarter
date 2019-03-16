74c6a9b53aff9aa66ba5a1c277c1b8f6648867dc
Laura Theriot <lauraftheriot@gmail.com>
2019-03-16 12:06:35 -0500

    getting book object back from book api and converting url to book title based on isbn

--- src/app/add_listing_modal/add_listing_modal.component.ts
+++ src/app/add_listing_modal/add_listing_modal.component.ts
@@ -12,6 +12,7 @@ export class AddListingModal implements OnInit {
 
   isbnVal: string = '';
   bookCondition: string = '';
+  title: string = '';
 
   constructor(public modal: ModalController, private http: HttpClient) { }
 
@@ -22,8 +23,23 @@ export class AddListingModal implements OnInit {
   addBook() {
     console.log(this.isbnVal);
     const bookCondition = this.bookCondition;
+    const isbnVal = this.isbnVal;
     console.log(bookCondition);
-    this.http.post('http://localhost:3000/user/listing', { params: this.isbnVal, bookCondition })
+
+    // get req to Open Library Books API for book title from isbn
+    this.http.get(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbnVal}&format=json`)
+      .subscribe(((bookInfo: any) => {
+        console.log(bookInfo);
+        // sends obj w url key where the end of the url is the book title separated by _
+        // grab just the title out of the url and switch _ to ' '
+        this.title = bookInfo[Object.keys(bookInfo)[0]].info_url
+        .split('/')[bookInfo[Object.keys(bookInfo)[0]].info_url.split('/').length - 1]
+        .split('_').join(' ');
+      }));
+
+    const title = this.title; // so that sending will not yield undefined
+
+    this.http.post('http://localhost:3000/user/listing', { params: this.isbnVal, bookCondition, title })
     .subscribe((allListings: any) => {
       console.log(allListings, 'ALL LISTINGS + NEW ONE');
     })
