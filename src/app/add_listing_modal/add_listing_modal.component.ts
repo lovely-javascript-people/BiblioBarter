e83bb1cc7ec7aeb90ae602c8b63d532f9b057180
Laura Theriot <lauraftheriot@gmail.com>
2019-03-16 11:09:57 -0500

    added condition input field for add listing modal. sending through params in req. console logging as expected.

--- src/app/add_listing_modal/add_listing_modal.component.ts
+++ src/app/add_listing_modal/add_listing_modal.component.ts
@@ -11,6 +11,7 @@ import { HttpClient } from '@angular/common/http';
 export class AddListingModal implements OnInit {
 
   isbnVal: string = '';
+  bookCondition: string = '';
 
   constructor(public modal: ModalController, private http: HttpClient) { }
 
@@ -20,7 +21,9 @@ export class AddListingModal implements OnInit {
 
   addBook() {
     console.log(this.isbnVal);
-    this.http.post('http://localhost:3000/user/listing', { params: this.isbnVal })
+    const bookCondition = this.bookCondition;
+    console.log(bookCondition);
+    this.http.post('http://localhost:3000/user/listing', { params: this.isbnVal, bookCondition })
     .subscribe((allListings: any) => {
       console.log(allListings, 'ALL LISTINGS + NEW ONE');
     })
