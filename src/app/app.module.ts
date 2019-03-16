1bb73a40a52506302c347e67af8fcfb85d4d8e92
Laura Theriot <lauraftheriot@gmail.com>
2019-03-15 22:40:27 -0500

    added and connected add listing modal to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,13 @@ import { HomePageModule } from './home/home.module';
 import { FooterModule } from './footer/footer.module';
 import { ModalsModule } from './modals/modals.module';
 import { SearchModalsModule } from './search_modal/search_modal.module';
+import { AddListingModule } from './add_listing_modal/add_listing_modal.module';
+// import { WantListModalModule } from './want_list_modal/want_list_modal.module';
 import { SettingsService } from './services/settings/settings.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,HomePageModule, FooterModule, ModalsModule, SearchModalsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, HomePageModule, FooterModule, ModalsModule, SearchModalsModule, AddListingModule],
   providers: [
     StatusBar,
     SplashScreen,
