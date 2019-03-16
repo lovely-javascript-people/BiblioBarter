df891c43bf909cad2bd32177fb01e32ef9142d7f
Laura Theriot <lauraftheriot@gmail.com>
2019-03-16 09:52:25 -0500

    fixed compiling issue in modals. functions set up that console.log input val as expected

--- src/app/add_listing_modal/add_listing_modal.module.ts
+++ src/app/add_listing_modal/add_listing_modal.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddListingModal } from './add_listing_modal.component';
+import { FormsModule } from '@angular/forms';
 import {IonicModule} from '@ionic/angular';
 
 @NgModule({
   declarations: [AddListingModal],
   imports: [
     IonicModule,
-    CommonModule
+    CommonModule,
+    FormsModule,
   ],
   entryComponents: [
     AddListingModal
