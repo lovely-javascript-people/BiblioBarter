ba915196dde7deff3ceb0c4ac6f0ebade6ea6e21
jmohon1986 <jmohon1986@gmail.com>
2019-03-18 09:13:17 -0500

    Successfully getting peer books from server

--- server/server.js
+++ server/server.js
@@ -250,6 +250,43 @@ app.get('/search/listing/isbn', (req, res) => {
 
 // GET /peer
 // returns wants for a profile you visit 
+app.get('/peer', (req, res) => {
+  let books;
+  let usersBooks = [];
+  let listings;
+  db.Want.findAll({
+    where: {
+      id_user: req.query.peerId
+    }
+  }).catch((err) => {
+    console.log(`error in peer wants: ${err}`);
+  }).then((peerWants) => {
+    books = peerWants;
+  }).then(() => {
+    db.Listing.findAll({
+      where: {
+        id_user: req.query.peerId
+      }
+    }).then((data) => {
+      listings = data;
+    }).then(() => {
+      books.push([]);
+      for (let listing of listings) {
+        books[books.length - 1].push(
+        db.Book.findOne({
+          where: {
+            id_book: listing.id_book
+          }
+        }))}
+        console.log(books[1]);
+        }).then(() => {
+      res.send(books);
+    })
+  }).catch((err) => {
+    console.log(`error in get peer wants: ${err}`);
+  });
+});
+
 // app.get('/peer', (req, res) => {
 //   let books;
 //   db.Want.findAll({
@@ -266,47 +303,23 @@ app.get('/search/listing/isbn', (req, res) => {
 //         id_user: req.query.peerId
 //       }
 //     }).then((data) => {
-//       books.push(data);
+//       // create array of just books using book ids
+//       let offeredbooks = data.map((listing) => {
+//         return db.Book.findAll({
+//           where: {
+//             id_book: listing.id_book
+//           },
+//           // include: [db.Listing]
+//         })
+//       })
+//       books.push(offeredbooks);
+      
 //       res.send(books);
 //     })
 //   }).catch((err) => {
 //     console.log(`error in get peer wants: ${err}`);
 //   });
 // });
-
-app.get('/peer', (req, res) => {
-  let books;
-  db.Want.findAll({
-    where: {
-      id_user: req.query.peerId
-    }
-  }).catch((err) => {
-    console.log(`error in peer wants: ${err}`);
-  }).then((peerWants) => {
-    books = peerWants;
-  }).then(() => {
-    db.Listing.findAll({
-      where: {
-        id_user: req.query.peerId
-      }
-    }).then((data) => {
-      // create array of just books using book ids
-      let offeredbooks = data.map((listing) => {
-        return db.Book.findAll({
-          where: {
-            id_book: listing.id_book
-          },
-          // include: [db.Listing]
-        })
-      })
-      books.push(offeredbooks);
-      
-      res.send(books);
-    })
-  }).catch((err) => {
-    console.log(`error in get peer wants: ${err}`);
-  });
-});
 
 
 
