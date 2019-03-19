91a4cc4cc337ac3a7f12f0476c7825879d99cef6
Laura Theriot <lauraftheriot@gmail.com>
2019-03-19 15:41:36 -0500

    code cleanup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -25,7 +25,6 @@ export class HomePage implements OnInit{
   }
 
   searchBooks(data, callback) {
-    // let isbn = this.isbnQuery;
     this.apiService.getBooks(data, callback);
   }
 
@@ -34,26 +33,18 @@ export class HomePage implements OnInit{
       this.listings = searchedListings;
     }
 
-  userMatches() {
-    //this.http.get(`//ec2-18-188-132-186.us-east-2.compute.amazonaws.com:3000/user/want?${localStorage.userid}`)
-    this.http.get(`http://localhost:3000/user/want?${localStorage.userid}`)
-    .subscribe((wantListArray) => {
-      console.log(wantListArray, 'ARRAY OF WANT LIST');
-      this.wants = wantListArray;
-      this.isbnQuery = wantListArray[0].isbn;
-      console.log(this.isbnQuery, 'ISBN');
-    })
-  //   const http = this.http;
-  //   const isbn = this.isbn;
-  //   console.log(isbn, 'TEST ISBN');
-  // http.get(`http://localhost:3000/search/listing/isbn?${isbn}`)
-  //   .subscribe((searchedListings: any) => {
-  //     console.log(this.isbn, 'HEY THIS IS THE ISBN HEY');
-  //     // console.log(searchedListings, 'AUTOSEARCH BOOKS');
-  //     this.listings = searchedListings;
+  // THIS DOESNT WORK YET --> SUPPOSED TO GRAB USER MATCHES ON INIT 
+  // userMatches() {
+  //   this.http.get(`http://localhost:3000/user/want?${localStorage.userid}`)
+  //   .subscribe((wantListArray) => {
+  //     console.log(wantListArray, 'ARRAY OF WANT LIST******');
+  //     this.wants = wantListArray;
+  //     this.isbnQuery = wantListArray[0].isbn;
+  //     console.log(this.isbnQuery, 'ISBN IN USER MATCHES');
   //   })
-    this.searchBooks();
-  }
+  //   let isbn = this.isbnQuery;
+  //   this.searchBooks(isbn, this.setListing);
+  // }
 
 
 
