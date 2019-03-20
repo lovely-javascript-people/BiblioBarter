ef50d756372b3c6ebbaf089d42fc91f3f3ad8961
Phuoc Nguyen <nguyen.phuoc23@gmail.com>
2019-03-19 19:37:35 -0500

    created barcode scanner, not currently working with emulator due to cordova error

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-
+import { NavController } from '@ionic/angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -14,8 +15,10 @@ export class HomePage implements OnInit{
   isbn: string = " ";
   listings: any = [];
   wants: any = [];
+  num: string; // stores the scanned result
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router, public navCtrl: NavController,
+    private barcodeScanner: BarcodeScanner) { }
 
   profileButtonClick(index) {
     console.log(this.listings[index]);
@@ -52,7 +55,20 @@ export class HomePage implements OnInit{
     this.searchBooks();
   }
 
-
+  // new scan method
+  scan() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      // this is called when a barcode is found
+      console.log(`barcode data: ${barcodeData}`);
+      console.log(("We got a barcode\n" +
+        "Result: " + barcodeData.text + "\n" +
+        "Format: " + barcodeData.format + "\n" +
+        "Cancelled: " + barcodeData.cancelled));
+      this.num = barcodeData.text
+    }).catch((err) => {
+      console.log(`error in barcode scan: ${err}`);
+    });
+  }
 
   ngOnInit() {
     // this.userMatches();
