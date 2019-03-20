79d3e7df3afa37c71f44e2f5ea983b55c1acac6a
jmohon1986 <jmohon1986@gmail.com>
2019-03-20 15:42:22 -0500

    create offer click now passing down correct info

--- src/app/profile/peerProfile/peer-profile/peer-profile.page.ts
+++ src/app/profile/peerProfile/peer-profile/peer-profile.page.ts
@@ -6,6 +6,7 @@ import { WantListModal } from '../../../want_list_modal/want_list_modal.componen
 import { AddListingModal } from '../../../add_listing_modal/add_listing_modal.component';
 import { ModalController } from '@ionic/angular';
 import { Router } from '@angular/router'
+import { _ } from 'underscore';
 
 @Component({
   selector: 'app-peer-profile',
@@ -21,7 +22,8 @@ export class PeerProfilePage implements OnInit {
   offers: object[];
   wants: any[];
   listings: any[];
-  possibleBooks: object[] = [];
+  entireListings: any[];
+  possibleBooks: any[] = [];
 
   constructor(private apiService: ApiService, public modal: ModalController, private router: Router,) {}
 
@@ -56,21 +58,33 @@ export class PeerProfilePage implements OnInit {
   }
 
   async setBooks(data) {
-    let temp = data.slice(0, data.length - 1)
+    console.log(data);
+    let temp = data.slice(0, 1)
     this.wants = temp;
-    this.listings = data[data.length - 1];
+    this.listings = data[1];
+    this.entireListings = data[2];
+    console.log(this.entireListings);
     this.getYourBooks();
     this.getYourWants();
   }
 
-  makeOffer(bookWanted: number, myOffer: number) {
-    this.apiService.sendOffer({ 
-      myId: this.me, 
-      myOffer, 
-      bookWanted: this.peer.listing.id_listing,
-      bookWantedTitle: this.peer.title,
-      peerId: this.peer.listing.id_user 
-    });
+  makeOffer(bookWanted: any, myOffer: any) {
+    let want = bookWanted.split('')
+    want[0] = '';
+    want[want.length - 1] = '';
+    let newWant = want.join('');
+    let off = myOffer.split('')
+    off[0] = '';
+    off[off.length - 1] = '';
+    let newOff = off.join('');
+    console.log(newOff);
+    let wanted = _.filter(this.listings, list => list.title === newWant);
+    let offered = this.possibleBooks[0].filter(book => newOff === book.book.title);
+    console.log(wanted, offered);
+    // this.apiService.sendOffer({ 
+    //   bookWanted: this.entireListings[0],
+    //   bookOffering: offered[0].id_listing,
+    // });
   }
 
   setUser(data) {
