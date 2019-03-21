5a4737bc0957a74f4414ef17a420f12995ba3237
Laura Theriot <lauraftheriot@gmail.com>
2019-03-20 20:38:41 -0500

    endpoint added for contact us modal

--- src/app/contact_modal/contact_modal.component.ts
+++ src/app/contact_modal/contact_modal.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { EmailComposer } from '@ionic-native/email-composer/ngx';
-import {NavController, ToastController} from '@ionic/angular';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+// import {NavController, ToastController} from '@ionic/angular';
+// import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { ApiService } from '../api.service';
 
 
 
@@ -14,35 +15,38 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 
 export class ContactModal implements OnInit {
 
-  // emailAddress: string = '';
-  // emailBody: string = '';
+  emailAddress: string = '';
+  emailBody: string = '';
 
-  available: boolean = true;
+  // available: boolean = true;
 
   
 
   constructor(
     public modal: ModalController,
-    private emailComposer: EmailComposer,
+    // private emailComposer: EmailComposer,
+    private apiService: ApiService
     ) {  }
     
-    send(){
-      this.emailComposer.isAvailable().then((available) =>{
-      if(available) {
-        //Now we know we can send    
-        console.log('sending email');  
-      }
-      });
-    }
+    // send(){
+    //   this.emailComposer.isAvailable().then((available) =>{
+    //   if(available) {
+    //     //Now we know we can send    
+    //     console.log('sending email');  
+    //   }
+    //   });
+    // }
 
 
 
 /////////////////////
-  // submitEmail() {
-  //   console.log(this.emailAddress);
-  //   console.log(this.emailBody);
-  //   this.closeModal();
-  // }
+  submitEmail() {
+    const userId = localStorage.userid;
+    const userEmail = this.emailAddress;
+    const emailBody = this.emailBody;
+    this.apiService.contactUs(userId, userEmail, emailBody);
+    this.closeModal();
+  }
 
   async closeModal() {
     this.modal.dismiss();
