ef83228911a539ea0dc35e027cf5b55113abc486
Phuoc Nguyen <nguyen.phuoc23@gmail.com>
2019-03-25 19:48:32 -0500

    /offer endpoint, creates offer with more than 1:1 book trade with money exchange

--- src/app/profile/peerProfile/peer-profile/peer-profile.page.ts
+++ src/app/profile/peerProfile/peer-profile/peer-profile.page.ts
@@ -69,24 +69,19 @@ export class PeerProfilePage implements OnInit {
   }
 
   makeOffer(bookWanted: any, myOffer: any) {
-    const want = bookWanted.split('');
-    want[0] = '';
-    want[want.length - 1] = '';
-    const newWant = want.join('');
-    const off = myOffer.split('');
-    off[0] = '';
-    off[off.length - 1] = '';
-    const newOff = off.join('');
-    // console.log(newOff);
-    const wanted = _.filter(this.listings, list => list.title === newWant);
-    const offered = this.possibleBooks[0].filter(book => newOff === book.book.title);
-    const wanting = this.entireListings.filter(listin => listin.id_book === wanted[0].id_book);
-    console.log(wanted, offered, 'WANTED, OFFERED');
-
+    debugger;
+    const allListings = [];
+    bookWanted.forEach((listing) => {
+      allListings.push(_.filter(this.listings, list => list.title === listing));
+    });
+    myOffer.forEach((listing) => {
+      allListings.push(_.filter(this.listings, list => list.title === listing));
+    });
+    
     let moneyOffer = (this.offeredMoney * -100);
     let moneyWant = (this.wantMoney * 100);
 
-    if(moneyOffer !== 0) {
+    if (moneyOffer !== 0) {
       this.money = moneyOffer;
     } else {
       this.money = moneyWant;
@@ -97,11 +92,47 @@ export class PeerProfilePage implements OnInit {
     // console.log(money, 'MONEY FOR EXCHANGE');
 
     this.apiService.sendOffer({
-      bookWanted: wanting,
-      bookOffering: offered[0].id_listing,
+      idRecipient: this.peer,
+      idSender: localStorage.userid,
       money: money,
+      listings: allListings,
     });
   }
+  // makeOffer(bookWanted: any, myOffer: any) {
+  //   debugger;
+  //   const want = bookWanted.split('');
+  //   want[0] = '';
+  //   want[want.length - 1] = '';
+  //   const newWant = want.join('');
+  //   const off = myOffer.split('');
+  //   off[0] = '';
+  //   off[off.length - 1] = '';
+  //   const newOff = off.join('');
+  //   // console.log(newOff);
+  //   const wanted = _.filter(this.listings, list => list.title === newWant);
+  //   const offered = this.possibleBooks[0].filter(book => newOff === book.book.title);
+  //   const wanting = this.entireListings.filter(listin => listin.id_book === wanted[0].id_book);
+  //   console.log(wanted, offered, 'WANTED, OFFERED');
+
+  //   let moneyOffer = (this.offeredMoney * -100);
+  //   let moneyWant = (this.wantMoney * 100);
+
+  //   if(moneyOffer !== 0) {
+  //     this.money = moneyOffer;
+  //   } else {
+  //     this.money = moneyWant;
+  //   }
+  //   // console.log(moneyOffer, 'MONEY OFFER');
+  //   // console.log(moneyWant, 'MONEY WANT');
+  //   let money = this.money;
+  //   // console.log(money, 'MONEY FOR EXCHANGE');
+
+  //   this.apiService.sendOffer({
+  //     bookWanted: wanting,
+  //     bookOffering: offered[0].id_listing,
+  //     money: money,
+  //   });
+  // }
 
   setUser(data) {
     const user: any = localStorage.selectedUser;
