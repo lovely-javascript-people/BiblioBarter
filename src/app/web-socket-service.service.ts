708d5a21eeb98e2c01469dfaf2fddb0f445d607d
jmohon1986 <jmohon1986@gmail.com>
2019-03-27 10:50:06 -0500

    Matches now filter by school and socket io bug fixed

--- src/app/web-socket-service.service.ts
+++ src/app/web-socket-service.service.ts
@@ -9,37 +9,37 @@ import * as Rx from 'rxjs/Rx';
 export class WebSocketServiceService {
 
   // Our socket connection
-  private socket;
+  // private socket;
 
   constructor() { }
-  connect(): Rx.Subject<MessageEvent> {
-    // Can use an environmental variable here
-    this.socket = io(`http://localhost:3000`);
+  // connect(): Rx.Subject<MessageEvent> {
+  //   // Can use an environmental variable here
+  //   this.socket = io(`http://localhost:3000`);
 
-    // We define our observable which will observe any incoming messages
-    // from our socket.io server.
-    const observable = new Observable(observer => {
-      this.socket.on('message', (data) => {
-        console.log('Received message from Websocket Server');
-        observer.next(data);
-      });
-      return () => {
-        this.socket.disconnect();
-      };
-    });
+  //   // We define our observable which will observe any incoming messages
+  //   // from our socket.io server.
+  //   const observable = new Observable(observer => {
+  //     this.socket.on('message', (data) => {
+  //       console.log('Received message from Websocket Server');
+  //       observer.next(data);
+  //     });
+  //     return () => {
+  //       this.socket.disconnect();
+  //     };
+  //   });
 
-    // We define our Observer which will listen to messages
-    // from our other components and send messages back to our
-    // socket server whenever the `next()` method is called.
-    const observer = {
-      next: (data: Object) => {
-        this.socket.emit('message', JSON.stringify(data));
-      },
-    };
+  //   // We define our Observer which will listen to messages
+  //   // from our other components and send messages back to our
+  //   // socket server whenever the `next()` method is called.
+  //   const observer = {
+  //     next: (data: Object) => {
+  //       this.socket.emit('message', JSON.stringify(data));
+  //     },
+  //   };
 
-    // we return our Rx.Subject which is a combination
-    // of both an observer and observable.
-    return Rx.Subject.create(observer, observable);
-  }
+  //   // we return our Rx.Subject which is a combination
+  //   // of both an observer and observable.
+  //   return Rx.Subject.create(observer, observable);
+  // }
 
 }
